64d7f0240833aab0673b29ce3f41c90a131df4d9
fuyutarow <fuyutarow@gmail.com>
2022-03-30 23:14:15 +0000

    Update content.js

--- target/content.js
+++ target/content.js
@@ -1,2 +1,8 @@
 paywall = document.querySelector('div[data-atlas-trackable="paywall"]');
 paywall.parentElement.removeChild(paywall);
+
+
+
+// https://www.slideshare.net/
+document.querySelector("#scribd-ad-exit-modal > div.scribd-ad-modal-wrapper").remove()
+document.querySelector('#new-player > div.limit-overlay').remove()
