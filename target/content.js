1b8b47262e1c18a8cc294dcc80a0b1d79040ede4
fuyutarow <fuyutarow@gmail.com>
2022-03-31 08:16:01 +0900

    For slideshare.net

--- target/content.js
+++ target/content.js
@@ -1,8 +1,12 @@
-paywall = document.querySelector('div[data-atlas-trackable="paywall"]');
-paywall.parentElement.removeChild(paywall);
+// paywall = document.querySelector('div[data-atlas-trackable="paywall"]');
+// paywall.parentElement.removeChild(paywall);
 
 
-
-// https://www.slideshare.net/
-document.querySelector("#scribd-ad-exit-modal > div.scribd-ad-modal-wrapper").remove()
-document.querySelector('#new-player > div.limit-overlay').remove()
+if (location.href.startsWith("https://www.slideshare.net")) {
+  const clean = () => {
+    document.querySelector("#scribd-ad-exit-modal > div.scribd-ad-modal-wrapper").remove()
+    document.querySelector('#new-player > div.limit-overlay').remove()
+  }
+  clean()
+  // setInterval(clean, 500);
+}
